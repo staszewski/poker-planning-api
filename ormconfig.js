90c902e59a2f7945a6ba90491159919c9c853f20
staszewski <kamilstaszewski95@gmail.com>
2021-06-06 18:16:32 +0200

    Successfully connect to db

---
+++ ormconfig.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+module.exports = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: 5432,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  synchronize: true,
+  dropSchema: false,
+  logging: true,
+  entities: ['dist/**/*.entity.js'],
+};
