1f260d03508e6f8428643cb5259fef078c13a2ee
staszewski <kamilstaszewski95@gmail.com>
2021-06-26 19:20:08 +0200

    Start learning about WS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { getMetadataArgsStorage } from 'typeorm';
+import { EventsModule } from './events/events.module';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require('../ormconfig');
 
@@ -18,8 +17,7 @@ const config = require('../ormconfig');
     }),
     UserModule,
     AuthModule,
+    EventsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useWebSocketAdapter(new WsAdapter(app)); // Add this line
+
   await app.listen(3000);
 }
 bootstrap();
