4e306bd42d8e93e81df8819ae9b8246fbc8a6843
staszewski <kamilstaszewski95@gmail.com>
2021-10-31 13:04:20 +0100

    getting of stupdi ws setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { getMetadataArgsStorage } from 'typeorm';
-import { EventsModule } from './events/events.module';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require('../ormconfig');
 
@@ -17,7 +16,6 @@ const config = require('../ormconfig');
     }),
     UserModule,
     AuthModule,
-    EventsModule,
   ],
 })
 export class AppModule {}
